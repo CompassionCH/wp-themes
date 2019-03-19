9baf38cf1a491b335db20d8972e84fad84a062d3
Christopher Meier <25900023+c-meier@users.noreply.github.com>
2019-03-19 10:31:04 +0100

    WP-30 : Fix agenda show dates

--- compassion/template-parts/content-agendas-teaser.php
+++ compassion/template-parts/content-agendas-teaser.php
@@ -5,45 +5,37 @@ $thumb = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_id()), 'thum
 ?>
 
 <article class="column large-3 medium-6 blog-teaser" data-equalizer-watch>
-
     <div class="content-wrapper">
 	<a href="<?php the_permalink(); ?>">
-    <div class="thumbnail" style="background-image: url(<?php echo $thumb[0]; ?>);"></div>
+        <div class="thumbnail" style="background-image: url(<?php echo $thumb[0]; ?>);"></div>
 
-      <h5 class="title"><?php the_title(); ?></h5>
+        <h5 class="title"><?php the_title(); ?></h5>
 
+        <?php if ( get_post_meta($post->ID, '_agenda_end_date', true) ) : ?>
+        <h5 class="date">
+            <?php _e('Vom', 'compassion'); ?>
+            <?php
+            $start_date = get_post_meta(get_the_id(), '_agenda_start_date', true);
+            echo date_create($start_date)->format('d.m.Y');
+            ?>
+            <?php _e('bis', 'compassion'); ?>
+            <?php
+            $end_date = get_post_meta(get_the_id(), '_agenda_end_date', true);
+            echo date_create($end_date)->format('d.m.Y');
+            ?>
+        </h5>
+        <?php else: ?>
+        <h5 class="date">
+            <?php
+            $start_date = get_post_meta(get_the_id(), '_agenda_start_date', true);
+            echo date_create($start_date)->format('d.m.Y');
+            ?>
+        </h5>
+        <?php endif ?>
 
-  <?php if ( get_post_meta(get_the_id(), '_agenda_end_date', true) ) : ?>
-      
-      <h5 class="date"><?php _e('Vom', 'compassion'); ?> <?php $text = get_post_meta(get_the_id(), '_agenda_start_date', true);
-	      echo date("d.m.Y", strtotime(str_replace('.', '-', $text)));?> <?php _e('bis', 'compassion'); ?> <?php  $text1 = get_post_meta(get_the_id(), '_agenda_end_date', true);
-	      echo date("d.m.Y", strtotime(str_replace('.', '-', $text1)));
-	      ?>
-	      
-	     </h5>
-	    <?php else: ?>
-	    
-	     <h5 class="date"><?php $text3 = get_post_meta(get_the_id(), '_agenda_start_date', true);
-	      echo date("d.m.Y", strtotime(str_replace('.', '-', $text3)));   ?>
-	   
-	  </h5>
-	    <?php endif ?>
-
-
-
-
-
-<!--
-
-         <h5 class="date"><?php $text = get_post_meta(get_the_id(), '_agenda_start_date', true);
-	      echo date("d.m.Y", strtotime(str_replace('.', '-', $text)));
-	      ?></h5>
--->
-      <div class="content">
-      <?php the_excerpt(); ?>
-      </div>
-  </a>
+        <div class="content">
+        <?php the_excerpt(); ?>
+        </div>
+    </a>
     </div>
-
-
 </article>
