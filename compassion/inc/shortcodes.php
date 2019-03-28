8bd1c6fdacebdb9548321b2d7d7ad3e55be093e6
Christopher Meier <25900023+c-meier@users.noreply.github.com>
2019-03-28 10:27:46 +0100

    WP-33 : Register the "button" shortcode in the ui

--- compassion/inc/shortcodes.php
+++ compassion/inc/shortcodes.php
@@ -1,15 +1,67 @@
 <?php
-
-function buttons($attrs, $content = null) {
-  extract(shortcode_atts(array(
-    'href' => '#',
-    'size' => 'large',
-    'color' => 'blue'
-  ), $attrs));
-
-  return '<a href="'.$href.'" class="button button-'.$color.' button-'.$size.'">'.$content.'</a>';
+function compassion_register_shortcode_button() {
+    add_shortcode('button', 'compassion_buttons');
+    if(function_exists('shortcode_ui_register_for_shortcode')) {
+        $args = array(
+            'label' => __('Insert button', 'compassion'),
+            'listItemImage' => 'dashicons-marker',
+            'inner_content' => array(
+                'label' => __('Button text', 'compassion'),
+                'description' => __('The text shown in the button.', 'compassion'),
+            ),
+            'attrs' => array(
+                array(
+                    'label' => __('Link', 'compassion'),
+                    'attr' => 'href',
+                    'description' => __('The URL to go to after clicking.', 'compassion'),
+                    'type' => 'url',
+                ),
+                array(
+                    'label' => __('Size', 'compassion'),
+                    'attr' => 'size',
+                    'description' => __('The size of the button.', 'compassion'),
+                    'type' => 'select',
+                    'options' => array(
+                        array('value' => 'small', 'label' => __('Small', 'compassion')),
+                        array('value' => 'medium', 'label' => __('Medium', 'compassion')),
+                        array('value' => 'large', 'label' => __('Large', 'compassion')),
+                    ),
+                ),
+                array(
+                    'label' => __('Color', 'compassion'),
+                    'attr' => 'color',
+                    'description' => __('The color of the button.', 'compassion'),
+                    'type' => 'select',
+                    'options' => array(
+                        array('value' => 'blue', 'label' => __('Blue', 'compassion')),
+                        array('value' => 'beige', 'label' => __('Beige', 'compassion')),
+                    ),
+                ),
+            ),
+        );
+        shortcode_ui_register_for_shortcode('button', $args);
+    }
 }
-add_shortcode('button', 'buttons');
+
+/**
+ * Called by the "button" shortcode.
+ */
+function compassion_buttons($attrs, $content = null) {
+    extract(shortcode_atts(array(
+        'href' => '#',
+        'size' => 'large',
+        'color' => 'blue'
+    ), $attrs));
+
+    return '<a href="'.$href.'" class="button button-'.$color.' button-'.$size.'">'.$content.'</a>';
+}
 //[button href="link" size="large"]text[/button]
 
-?>
+
+/**
+ * Function to register all the shortcode of the theme.
+ */
+function compassion_shortcode_init() {
+  compassion_register_shortcode_button();
+}
+add_action('init', 'compassion_shortcode_init');
