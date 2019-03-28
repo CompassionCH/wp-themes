8cd6312d61191615bcbbba6ee9a26f1c1e84a7c5
Christopher Meier <25900023+c-meier@users.noreply.github.com>
2019-03-28 10:07:27 +0100

    WP-33 : Remove unused button_analytics shortcode

--- compassion/inc/shortcodes.php
+++ compassion/inc/shortcodes.php
@@ -12,21 +12,4 @@ function buttons($attrs, $content = null) {
 add_shortcode('button', 'buttons');
 //[button href="link" size="large"]text[/button]
 
-function button_ga($attrs, $content = null) {
-  extract(shortcode_atts(array(
-    'href' => '#',
-    'send' => 'send',
-    'event' => 'event',
-    'cat' => 'cat',
-    'action' => 'action',
-    'label' => 'label',
-    'value' => 'value',
-    'size' => 'large',
-    'color' => 'blue'
-  ), $attrs));
-
-  return '<a onClick="ga('.$send.' , '.$event.' , '.$cat.' , '.$action.' , '.$label.' , '.$value.');" href="'.$href.'" class="button button-'.$color.' button-'.$size.'">'.$content.'</a>';
-}
-add_shortcode('button_analytics', 'button_ga');
-
 ?>
