12f49b27fc17243b9e4f78b9c088873eea600501
olivier requet <orequet@compassion.ch>
2021-06-01 10:09:39 +0200

    new childpool style

--- compassion/template-parts/content-child-teaser.php
+++ compassion/template-parts/content-child-teaser.php
@@ -2,19 +2,23 @@
 
 $thumb = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_id()), 'thumbnail_size' );
 //ModifJK
-$thumb[0] = str_replace('z_0.6','z_0.8,q_70',(str_replace('w_320','w_609',str_replace('h_420','h_205',$thumb[0]))));
+$thumb[0] = str_replace('z_0.6','z_0.8,q_70',(str_replace('w_320','w_400',str_replace('h_420','h_400',$thumb[0]))));
 $child_meta = get_child_meta(get_the_id());
 ?>
 
-<article class="column medium-6 large-3 blog-teaser child-teaser child" data-equalizer-watch>
+<article class="column medium-6 large-4 blog-teaser child-teaser child" data-equalizer-watch>
   <div class="content-wrapper">
+
+      <div class="thumb_cont">
  <a href="<?php the_permalink(); ?>">
     <div class="thumbnail" style="background-image: url(<?php echo str_replace('w_270,','w_480,',$thumb[0]); ?>); background-position:center center; ">
     </div>
  </a>
+    </div>
+      <!--
     <span class="child-waiting"><?php echo $child_meta['waiting_days']; ?></span>
 
-    <!--
+
     <div class="single-icon icon-small">
       <object type="image/svg+xml"
               data="<?php bloginfo('template_directory'); ?>/assets/img/person.svg">
@@ -29,14 +33,14 @@ $child_meta = get_child_meta(get_the_id());
         <h4 class="title svg-divider"><?php the_title(); ?></h4>
 
         <ul>
-          <li><strong><?php _e('Land', 'compassion'); ?> </strong><?php echo $child_meta['country']; ?></li>
-          <li><strong><?php _e('Alter', 'compassion'); ?> </strong><?php echo $child_meta['age']; ?> <?php _e('Jahre', 'compassion'); ?></li>
-          <li><strong><?php _e('Geschlecht', 'compassion'); ?> </strong><?php echo $child_meta['gender']; ?></li>
+          <li><!-- <strong>--> <?php /*_e('Land', 'compassion'); */?><!-- </strong>--><?php echo $child_meta['age']; ?> <?php _e('Jahre', 'compassion'); ?> | <?php echo $child_meta['gender']; ?> | <?php echo $child_meta['country']; ?></li>
+<!--            <li><strong>--><?php //('Alter', 'compassion');?><!--</strong>--><?php //echo $child_meta['age']; ?><!-- --><?php //_e('Jahre', 'compassion'); ?><!--</li>-->
+<!--          <li><strong>--><?php //_e('Geschlecht', 'compassion'); ?><!--</strong>--><?php //echo $child_meta['gender']; ?><!--</li>-->
         </ul>
 
-        <div class="hover-overlay">
-          <h5 class="text-uppercase"><?php _e('Erfahre', 'compassion'); echo '<br />'; _e('mehr über', 'compassion'); ?><br /><?php the_title(); ?></h5>
-        </div>
+      <!--  <div class="hover-overlay">
+          <h5 class="text-uppercase"><?php /*_e('Erfahre', 'compassion'); echo '<br />'; _e('mehr über', 'compassion'); */?><br /><?php /*the_title(); */?></h5>
+        </div>-->
       </a>
 
     </div>
