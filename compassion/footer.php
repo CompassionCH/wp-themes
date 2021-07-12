ef4b6a1b40571c446e054d9ef8c0244e4032faeb
olivier requet <orequet@compassion.ch>
2021-07-12 09:58:33 +0200

    update footer compassion

--- compassion/footer.php
+++ compassion/footer.php
@@ -8,7 +8,7 @@ global $post;
 
 <!--<footer class="section background-blue section_abgerissen_oben section_full_width">-->
 
-    <div class="row" data-equalizer data-equalize-on="medium">
+    <div class="row expanded" data-equalizer data-equalize-on="medium">
 
         <div class="large-3 medium-3 column" data-equalizer-watch>
             <?php dynamic_sidebar('Footer Spalte 1'); ?>
@@ -39,8 +39,17 @@ global $post;
                     <?php echo get_theme_mod("kontakt-plz"); ?> <?php echo get_theme_mod("kontakt-ort"); ?><br />
                     <?php _e('Tel.', 'compassion') ?>: <?php echo get_theme_mod("kontakt-tel"); ?><br />
                     <?php _e('Email', 'compassion'); ?>: <a href="mailto:<?php echo get_theme_mod("kontakt-email"); ?>"><?php echo get_theme_mod("kontakt-email"); ?></a><br />
-                    <?php _e('IBAN', 'compassion'); ?> <?php echo get_theme_mod("bankverbindung-iban"); ?><br />
-
+                    <?php _e('IBAN', 'compassion'); ?> <?php echo get_theme_mod("bankverbindung-iban"); ?><br /><br />
+                   
+                    <?php if(ICL_LANGUAGE_CODE=='fr') { ?>
+                		<?php _e('<a href="/donations/">Vos dons</a> à Compassion sont déductibles des impôts.', 'compassion'); ?>
+					<?php }elseif(ICL_LANGUAGE_CODE=='de'){ ?>
+					 	<?php _e('<a href="/de/spenden/">Deine Spende</a> an Compassion ist steuerabzugsberechtigt.', 'compassion'); ?>
+					<?php }elseif(ICL_LANGUAGE_CODE=='it'){ ?>
+                    	<?php _e('<a href="/it/donare/">Le tue donazioni</a> a Compassion sono deducibili dalle tasse.', 'compassion'); ?>
+					<?php } ?>
+					
+					
                 </p>
             </div>
             <!--						<div class="footer-bank">
@@ -62,7 +71,7 @@ global $post;
                 if(ICL_LANGUAGE_CODE=='fr') { ?>
 
                     <?php $imagepng = get_template_directory_uri().'/assets/img/unis-' .ICL_LANGUAGE_CODE.".png"?>
-                    <a href="https://pour-les-enfants.ch" target="_blank"> <img src="<?php echo $imagepng?>"> </a>
+                    <a href="#"> <img src="<?php echo $imagepng?>"> </a>
 
                 <?php }elseif(ICL_LANGUAGE_CODE=='de'){ ?>
 
@@ -82,8 +91,8 @@ global $post;
 
     </div>
 
-    <div class="sub-footer row">
-        <div class="medium-12 column border">
+    <div class="sub-footer row expanded">
+        <div class="medium-12 column">
             <div class="medium-4 column">
                 © <?php echo date('Y', strtotime('now')); ?> <?php _e('Compassion Schweiz', 'compassion'); ?>
             </div>
@@ -102,24 +111,6 @@ global $post;
 </div> <!-- .off-canvas-wrapper-inner -->
 
 </div> <!-- .off-canvas-wrapper -->
-
-<!-- Facebook Pixel Code -->
-<script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '555270392517284');
-    fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-               src="https://www.facebook.com/tr?id=555270392517284&ev=PageView&noscript=1"
-    /></noscript>
-<!-- End Facebook Pixel Code -->
 
 <?php wp_footer(); ?>
 
