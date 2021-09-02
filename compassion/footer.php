5eef8a85030542b2f5c97a5ef65f06a6fdba70ff
olivier requet <orequet@compassion.ch>
2021-09-02 09:46:05 +0200

    correct e-mail in german

--- compassion/footer.php
+++ compassion/footer.php
@@ -39,7 +39,7 @@ global $post;
                     <?php echo get_theme_mod("kontakt-strasse"); ?><br />
                     <?php echo get_theme_mod("kontakt-plz"); ?> <?php echo get_theme_mod("kontakt-ort"); ?><br />
                     <?php _e('Tel.', 'compassion') ?>: <?php echo get_theme_mod("kontakt-tel"); ?><br />
-                    <?php _e('Email', 'compassion'); ?>: <a href="mailto:<?php echo get_theme_mod("kontakt-email"); ?>"><?php echo get_theme_mod("kontakt-email"); ?></a><br />
+                    <?php _e('E-mail', 'compassion'); ?>: <a href="mailto:<?php echo get_theme_mod("kontakt-email"); ?>"><?php echo get_theme_mod("kontakt-email"); ?></a><br />
                     <?php _e('IBAN', 'compassion'); ?> <?php echo get_theme_mod("bankverbindung-iban"); ?><br /><br />
                    
                     <?php if(ICL_LANGUAGE_CODE=='fr') { ?>
