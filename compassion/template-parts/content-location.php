7be2090d7c98cd5aabb57af97d878f0fb3448c9f
olivier compassion <orequet@compassion.ch>
2026-07-20 16:13:40 +0200

    change sponsor page for country

--- compassion/template-parts/content-location.php
+++ compassion/template-parts/content-location.php
@@ -8,6 +8,6 @@ the_content();
     </div>
     <div class="row text-center">
 
-        <a href="<?php echo get_the_permalink(get_theme_mod("children-archive")); ?>?my_gender=&my_age_slot=0-50&my_country=<?php echo the_ID();?>" class="button button-blue button-medium"><?php _e('Werde Pate von einem Kind', 'compassion') ?> <?php echo get_post_meta(get_the_id(), '_cmb_country_child_title', true)?></a>
+        <a href="<?php echo get_the_permalink(get_theme_mod("pate-werden")); ?>?my_gender=&my_age_slot=0-50&my_country=<?php echo the_ID();?>" class="button button-blue button-medium"><?php _e('Werde Pate von einem Kind', 'compassion') ?> <?php echo get_post_meta(get_the_id(), '_cmb_country_child_title', true)?></a>
     </div>
 </div>
